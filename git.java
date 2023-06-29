155b54dac680019a4d6fc356e36ec53374c53c71
devops-b-01 <indore.devops@gmail.com>
2023-06-29 11:38:29 +0530

    java file updated

--- git.java
+++ git.java
@@ -1,6 +1,6 @@
 git init
 git add README.md
-git commit -m "first commit"
+git commit -m "second commit"
 git branch -M main
 git remote add origin https://github.com/cdec-b1/zomato.git
 git push -u origin main
